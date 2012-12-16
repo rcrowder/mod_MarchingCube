8bbc609065d2dd83f34280a476a8630e43944da1
rcrowder <riccro@gmail.com>
2012-12-16 22:08:48 +0000

    Moving class path

--- src/common/mod_MarchingCube/common/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package mod_MarchingCube.common;
-
-public class CommonProxy
-{
-    public void registerRenderThings()
-    {
-        // un-used server side -- see client proxy for implementation
-    }
-}

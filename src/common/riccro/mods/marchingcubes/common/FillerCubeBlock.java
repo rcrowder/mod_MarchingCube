78e7d2167d9be762eec51392f910ca86271eea59
rcrowder <riccro@gmail.com>
2012-12-22 13:34:33 +0000

    Added canPlaceBlockAt to filler block

--- src/common/riccro/mods/marchingcubes/common/FillerCubeBlock.java
+++ src/common/riccro/mods/marchingcubes/common/FillerCubeBlock.java
@@ -3,9 +3,7 @@ package riccro.mods.marchingcubes.common;
 import net.minecraft.src.Block;
 import net.minecraft.src.ChunkCoordinates;
 import net.minecraft.src.CreativeTabs;
-import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Material;
-import net.minecraft.src.Tessellator;
 import net.minecraft.src.World;
 import riccro.mods.marchingcubes.client.RenderMarchingCubes;
 import cpw.mods.fml.common.FMLLog;
@@ -16,55 +14,97 @@ public class FillerCubeBlock extends Block
 	{
 		super(id, texture, Material.ground);
 		this.setCreativeTab(CreativeTabs.tabBlock);	
+
+		neighborChanged = false;
 	}
 
-    /**
-     * The type of render function that is called for this block
-     */
+	/**
+	 * The type of render function that is called for this block
+	 */
 	@Override
 	public int getRenderType()
-    {
-        return RenderMarchingCubes.renderId;
-    }
-    
-	/**
-     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
-     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
-     */
-	@Override
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
-
-    /**
-     * Returns if this block is collidable (only used by Fire). Args: x, y, z
-     */
-	@Override
-    public boolean isCollidable()
-    {
-        return true;
-    }
-    
-    /**
-     * Called whenever the block is added into the world. Args: world, x, y, z
-     */
-	@Override
-    public void onBlockAdded(World par1World, int par2, int par3, int par4)
 	{
+		return RenderMarchingCubes.renderId;
+	}
+
+	/**
+	 * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+	 * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+	 */
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	/**
+	 * Returns if this block is collidable (only used by Fire). Args: x, y, z
+	 */
+	@Override
+	public boolean isCollidable()
+	{
+		return true;
+	}
+
+	public static ChunkCoordinates[] faceOffsets = 
+		{
+		new ChunkCoordinates(-1,  0,  0),	// Left
+		new ChunkCoordinates( 1,  0,  0),	// Right
+		new ChunkCoordinates( 0,  1,  0),	// Up
+		new ChunkCoordinates( 0, -1,  0),	// Down
+		new ChunkCoordinates( 0,  0,  1),	// Forward
+		new ChunkCoordinates( 0,  0, -1)	// Back
+		};
+
+	/**
+	 * Checks to see if its valid to put this block at the specified coordinates. Args: world, x, y, z
+	 */
+	@Override
+	public boolean canPlaceBlockAt(World par1World, int par2, int par3, int par4)
+	{
+		int count = 0;
+		int thisCount = 0;
+
+		for (int i = faceOffsets.length-1; i >= 0; i--)
+		{
+			int var5 = par1World.getBlockId(par2+faceOffsets[i].posX, par3+faceOffsets[i].posY, par4+faceOffsets[i].posZ);
+
+			if ((var5 > 0 && !blocksList[var5].blockMaterial.isGroundCover()))
+				count++;
+			
+			if (var5 == blockID)
+				thisCount++;
+		}
+
+		return count > thisCount;
+	}
+
+	/**
+	 * Called whenever the block is added into the world. Args: world, x, y, z
+	 */
+	@Override
+	public void onBlockAdded(World par1World, int par2, int par3, int par4)
+	{
+		neighborChanged = false;
+
 		FMLLog.info("onBlockAdded [%d %d %d]", par2, par3, par4);
 	}
 
-    /**
-     * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
-     * their own) Args: x, y, z, neighbor blockID
-     */
+	/**
+	 * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
+	 * their own) Args: x, y, z, neighbor blockID
+	 */
 	@Override
-    public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5)
-    {
+	public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, int par5)
+	{
+		neighborChanged = true;
+
 		if (par5 == blockID)
 			FMLLog.info("onNeighborBlockChange [%d %d %d] MCB", par2, par3, par4);
 		else
 			FMLLog.info("onNeighborBlockChange [%d %d %d] %d", par2, par3, par4, par5);
-    }
+	}
+
+	public boolean neighborChanged;
+
 }
--- src/minecraft/riccro/mods/marchingcubes/client/RenderMarchingCubes.java
+++ src/minecraft/riccro/mods/marchingcubes/client/RenderMarchingCubes.java
@@ -15,229 +15,232 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class RenderMarchingCubes implements ISimpleBlockRenderingHandler
 {
-    public static int renderId;
+	public static int renderId;
 
-    public RenderMarchingCubes(int renderId)
-    {
-        this.renderId = renderId;
-    }
+	public RenderMarchingCubes(int renderId)
+	{
+		this.renderId = renderId;
+	}
 
-    @Override
-    public int getRenderId()
-    {
-        return renderId;
-    }
+	@Override
+	public int getRenderId()
+	{
+		return renderId;
+	}
 
 	public static ChunkCoordinates[][] faceOffsets = 
 		{
-			// Forward first (back-spin like a foosball player)
-			{
-				new ChunkCoordinates( 0,  0,  1),	// Forward
-				new ChunkCoordinates( 0,  1,  1),
-				new ChunkCoordinates( 0,  1,  0),	// Up
-				new ChunkCoordinates( 0,  1, -1),
-				new ChunkCoordinates( 0,  0, -1),	// Back
-				new ChunkCoordinates( 0, -1, -1),
-				new ChunkCoordinates( 0, -1,  0),	// Down
-				new ChunkCoordinates( 0, -1,  1)
-			},
-			// Left first (spin clockwise like a Catherine wheel) 
-			{
-				new ChunkCoordinates(-1,  0,  0),	// Left
-				new ChunkCoordinates(-1,  1,  0),
-				new ChunkCoordinates( 0,  1,  0),	// Up
-				new ChunkCoordinates( 1,  1,  0),
-				new ChunkCoordinates( 1,  0,  0),	// Right
-				new ChunkCoordinates( 1, -1,  0),
-				new ChunkCoordinates( 0, -1,  0),	// Down
-				new ChunkCoordinates(-1, -1,  0)
-			},
-			// Forward first (rotate clockwise on the spot)
-			{
-				new ChunkCoordinates( 0,  0,  1),	// Forward
-				new ChunkCoordinates( 1,  0,  1),
-				new ChunkCoordinates( 1,  0,  0),	// Right
-				new ChunkCoordinates( 1,  0, -1),
-				new ChunkCoordinates( 0,  0, -1),	// Back
-				new ChunkCoordinates(-1,  0, -1),
-				new ChunkCoordinates(-1,  0,  0),	// Left
-				new ChunkCoordinates(-1,  0,  1)
-			},
+		// Forward first (back-spin like a foosball player)
+		{
+			new ChunkCoordinates( 0,  0,  1),	// Forward
+			new ChunkCoordinates( 0,  1,  1),
+			new ChunkCoordinates( 0,  1,  0),	// Up
+			new ChunkCoordinates( 0,  1, -1),
+			new ChunkCoordinates( 0,  0, -1),	// Back
+			new ChunkCoordinates( 0, -1, -1),
+			new ChunkCoordinates( 0, -1,  0),	// Down
+			new ChunkCoordinates( 0, -1,  1)
+		},
+		// Left first (spin clockwise like a Catherine wheel) 
+		{
+			new ChunkCoordinates(-1,  0,  0),	// Left
+			new ChunkCoordinates(-1,  1,  0),
+			new ChunkCoordinates( 0,  1,  0),	// Up
+			new ChunkCoordinates( 1,  1,  0),
+			new ChunkCoordinates( 1,  0,  0),	// Right
+			new ChunkCoordinates( 1, -1,  0),
+			new ChunkCoordinates( 0, -1,  0),	// Down
+			new ChunkCoordinates(-1, -1,  0)
+		},
+		// Forward first (rotate clockwise on the spot)
+		{
+			new ChunkCoordinates( 0,  0,  1),	// Forward
+			new ChunkCoordinates( 1,  0,  1),
+			new ChunkCoordinates( 1,  0,  0),	// Right
+			new ChunkCoordinates( 1,  0, -1),
+			new ChunkCoordinates( 0,  0, -1),	// Back
+			new ChunkCoordinates(-1,  0, -1),
+			new ChunkCoordinates(-1,  0,  0),	// Left
+			new ChunkCoordinates(-1,  0,  1)
+		},
 		};
 
-    public static void fillerBlockRenderer(Tessellator var5, int planeIndex, IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
-    {
-        // TODO: Neighbour colours
-        //var5.setColorOpaque_I(block.getBlockColor());
-        
-        int var6 = block.blockIndexInTexture;
+	public static void fillerBlockRenderer(Tessellator var5, int planeIndex, IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+	{
+		// TODO: Neighbour colours
+		//var5.setColorOpaque_I(block.getBlockColor());
 
-        int var7 = (var6 & 15) << 4;
-        int var8 = var6 & 240;
+		int var6 = block.blockIndexInTexture;
 
-        for (int faceBlockCount = 0; faceBlockCount < faceOffsets[planeIndex].length; faceBlockCount += 2)
-        {
-        	boolean fillerCubeFound = false;
-        	
-        	int edgeBlockCount = 0;
-        	
-    		float xOffset = 0.0f;
-    		float yOffset = 0.0f;
-    		float zOffset = 0.0f;
-    		
-    		int edgeBlockIndex = 0;
-        	for (; edgeBlockIndex >= -2; edgeBlockIndex--)
-        	{
-        		int offsetIndex = faceBlockCount + edgeBlockIndex;
-        		
-        		if (offsetIndex < 0)
-        			offsetIndex += faceOffsets[planeIndex].length;
-        		
-        		if (offsetIndex >= faceOffsets[planeIndex].length)
-        			offsetIndex -= faceOffsets[planeIndex].length;
-       			
-        		xOffset += faceOffsets[planeIndex][offsetIndex].posX;
-        		yOffset += faceOffsets[planeIndex][offsetIndex].posY;
-        		zOffset += faceOffsets[planeIndex][offsetIndex].posZ;
-        		
-                int id = world.getBlockId(	x + faceOffsets[planeIndex][offsetIndex].posX, 
-            								y + faceOffsets[planeIndex][offsetIndex].posY,
-            								z + faceOffsets[planeIndex][offsetIndex].posZ);
-                
-               	if (id > 0 && Block.blocksList[id] != null)
-               	{
-               		if (Block.opaqueCubeLookup[id])
-               			edgeBlockCount++;
-               	
-           			if (id == marchingcubes.instance.fillerCubeBlock.blockID)
-           				fillerCubeFound = true;
-               	}
-        	}
+		int var7 = (var6 & 15) << 4;
+		int var8 = var6 & 240;
 
-        	if (edgeBlockCount > 0 && edgeBlockCount < 3)
-        	{
-        		// Normalise offset vector
-        		xOffset /= 4.0;
-        		yOffset /= 4.0;
-        		zOffset /= 4.0;
-                
-                double var10 = (double)((float)var7 / 256.0F);
-                double var11 = (double)(((float)var7 + 7.99F) / 256.0F);
-                double var12 = (double)(((float)var7 + 15.99F) / 256.0F);
-                double var14 = (double)((float)var8 / 256.0F);
-                double var15 = (double)(((float)var8 + 7.99F) / 256.0F);
-                double var16 = (double)(((float)var8 + 15.99F) / 256.0F);
+		double var10 = (double)((float)var7 / 256.0F);
+		double var11 = (double)(((float)var7 + 7.99F) / 256.0F);
+		double var12 = (double)(((float)var7 + 15.99F) / 256.0F);
+		double var14 = (double)((float)var8 / 256.0F);
+		double var15 = (double)(((float)var8 + 7.99F) / 256.0F);
+		double var16 = (double)(((float)var8 + 15.99F) / 256.0F);
 
-		        // Block centre
-	            var5.addVertexWithUV(x + 0.5, y + 0.5, z + 0.5, var11, var15);
-	            
-	            if (!fillerCubeFound && edgeBlockCount < 3) 
-	            {		            
-		            if (planeIndex == 0)
-		            {
-		            	// YZ Plane
-				        var5.addVertexWithUV(x,       y + 0.5 + yOffset, z + 0.5 + zOffset, var10, var15 - ((yOffset * 16.0f) / 256.0f));
-				        var5.addVertexWithUV(x + 1.0, y + 0.5 + yOffset, z + 0.5 + zOffset, var12, var15 - ((yOffset * 16.0f) / 256.0f));
-		            }
-		            else
-		            if (planeIndex == 1)
-		            {
-		            	// XY Plane
-				        var5.addVertexWithUV(x + 0.5 + xOffset, y + 0.5 + yOffset, z,       var12, var15 - ((yOffset * 16.0f) / 256.0f));
-				        var5.addVertexWithUV(x + 0.5 + xOffset, y + 0.5 + yOffset, z + 1.0, var10, var15 - ((yOffset * 16.0f) / 256.0f));
-		            }
-		            else
-		            {
-		            	// XZ Plane
-				        var5.addVertexWithUV(x + 0.5 + xOffset, y,       z + 0.5 + zOffset, var11 + ((xOffset * 16.0f) / 256.0f), var16);
-				        var5.addVertexWithUV(x + 0.5 + xOffset, y + 1.0, z + 0.5 + zOffset, var11 + ((xOffset * 16.0f) / 256.0f), var14);
-		            }
-	            }
-	            else
-	            {
-	            	/*
-		            if (offsetIndex == 0)
-		            {
-				        var5.addVertexWithUV(x,       y + 0.5 + yOffset/2, z + 0.5 + zOffset/2, var12, var14);
-				        var5.addVertexWithUV(x + 0.5, y + 0.5 + yOffset/2, z + 0.5 + zOffset/2, var12, var16);
-		            }
-		            else
-		            if (offsetIndex == 1)
-		            {
-				        var5.addVertexWithUV(x + 0.5 + xOffset/2, y + 0.5 + yOffset/2, z,       var12, var14);
-				        var5.addVertexWithUV(x + 0.5 + xOffset/2, y + 0.5 + yOffset/2, z + 0.5, var12, var16);
-		            }
-		            else
-		            {
-				        var5.addVertexWithUV(x + 0.5 + xOffset/2, y,       z + 0.5 + zOffset/2, var12, var14);
-				        var5.addVertexWithUV(x + 0.5 + xOffset/2, y + 0.5, z + 0.5,           var12, var16);
-		            }
-	            	*/
-	            }
-        	}
-        }
-   		
-        //getFirstUncoveredBlock
-    }
+		// Loop through blocks adjacent to this block (i.e. adjacent face planes matching this blocks face planes)
+		for (int faceBlockCount = 0; faceBlockCount < faceOffsets[planeIndex].length; faceBlockCount += 2)
+		{
+			boolean fillerCubeAdjacent = false;
 
-    public static void marchingCubeBlockRenderer(Tessellator var5, int offsetIndex, IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
-    {
-    	
-    }
+			int edgeBlockCount = 0;
 
-    @Override
-    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
-    {
-    	Tessellator var5 = Tessellator.instance;
-        if (var5.isDrawing)
-        {
-        	// Call draw() to clear out any existing block drawing.
-        	var5.draw();
-        }
+			// Direction vector to the common edge (from centre of our block)
+			double xOffset = 0.0;
+			double yOffset = 0.0;
+			double zOffset = 0.0;
 
-        int mode = var5.drawMode;
+			// Loop around the common edge
+			for (int edgeBlockIndex = 0; edgeBlockIndex >= -2; edgeBlockIndex--)
+			{
+				int offsetIndex = faceBlockCount + edgeBlockIndex;
 
-        var5.startDrawing(GL11.GL_TRIANGLES);
-        
-        GL11.glDisable(GL11.GL_CULL_FACE);
+				if (offsetIndex < 0)
+					offsetIndex += faceOffsets[planeIndex].length;
 
-        if (block.blockID == marchingcubes.instance.fillerCubeBlock.blockID)
-        {
-        	// YZ Plane
-        	fillerBlockRenderer(var5, 0, world, x, y, z, block, modelId, renderer);
-        	
-        	// XY Plane
-        	fillerBlockRenderer(var5, 1, world, x, y, z, block, modelId, renderer);
-        	
-        	// XZ Plane
-        	fillerBlockRenderer(var5, 2, world, x, y, z, block, modelId, renderer);
-        }
-        else
-        if (block.blockID == marchingcubes.instance.marchingCubeBlock.blockID)
-        {
-        	marchingCubeBlockRenderer(var5, 0, world, x, y, z, block, modelId, renderer);
-        }
-        
-        var5.draw();
+				if (!fillerCubeAdjacent)
+				{
+					xOffset += faceOffsets[planeIndex][offsetIndex].posX;
+					yOffset += faceOffsets[planeIndex][offsetIndex].posY;
+					zOffset += faceOffsets[planeIndex][offsetIndex].posZ;
+				}
+				
+				int id = world.getBlockId(
+						x + faceOffsets[planeIndex][offsetIndex].posX, 
+						y + faceOffsets[planeIndex][offsetIndex].posY,
+						z + faceOffsets[planeIndex][offsetIndex].posZ);
 
-        GL11.glEnable(GL11.GL_CULL_FACE);
+				if (id > 0 && Block.blocksList[id] != null)
+				{
+					if (Block.opaqueCubeLookup[id])
+						edgeBlockCount++;
 
-        // After changing to GL_TRIANGLES, we need 
-        // to change back to the previous draw mode
-        var5.startDrawing(mode);
-        return true;
-    }
+					// Filler blocks are opaque, so could them here
+					if (id == marchingcubes.instance.fillerCubeBlock.blockID)
+					{
+						edgeBlockCount++;
+						fillerCubeAdjacent = true;
+					}
+				}
+			}
 
-    @Override
-    public boolean shouldRender3DInInventory()
-    {
-        return false;
-    }
+			// At least one block around our common edge?
+			if (edgeBlockCount > 0)
+			{
+				if (edgeBlockCount < 3 && !fillerCubeAdjacent) 
+				{		            
+					// Normalise offset vector
+					xOffset /= 4.0;
+					yOffset /= 4.0;
+					zOffset /= 4.0;
+					
+					// Block centre
+					var5.addVertexWithUV(x + 0.5, y + 0.5, z + 0.5, var11, var15);
 
-    @Override
-    public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer)
-    {
+					if (planeIndex == 0)
+					{
+						// YZ Plane
+						var5.addVertexWithUV(x,       y + 0.5 + yOffset, z + 0.5 + zOffset, 
+								var10, var15 - ((yOffset * 16.0f) / 256.0f));
+						
+						var5.addVertexWithUV(x + 1.0, y + 0.5 + yOffset, z + 0.5 + zOffset, 
+								var12, var15 - ((yOffset * 16.0f) / 256.0f));
+					}
+					else
+						if (planeIndex == 1)
+						{
+							// XY Plane
+							var5.addVertexWithUV(x + 0.5 + xOffset, y + 0.5 + yOffset, z,       
+									var12, var15 - ((yOffset * 16.0f) / 256.0f));
+							
+							var5.addVertexWithUV(x + 0.5 + xOffset, y + 0.5 + yOffset, z + 1.0, 
+									var10, var15 - ((yOffset * 16.0f) / 256.0f));
+						}
+						else
+						{
+							// XZ Plane
+							var5.addVertexWithUV(x + 0.5 + xOffset, y,       z + 0.5 + zOffset, 
+									var11 + ((xOffset * 16.0f) / 256.0f), var16);
+							
+							var5.addVertexWithUV(x + 0.5 + xOffset, y + 1.0, z + 0.5 + zOffset, 
+									var11 + ((xOffset * 16.0f) / 256.0f), var14);
+						}
+				}
+				else
+					if (edgeBlockCount < 3 && fillerCubeAdjacent)
+					{
+						// Block centre
+						//var5.addVertexWithUV(x + 0.5, y + 0.5, z + 0.5, var11, var15);
 
-    }
-	
+					}
+			}
+		}
+
+		//getFirstUncoveredBlock
+	}
+
+	public static void marchingCubeBlockRenderer(Tessellator var5, int offsetIndex, IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+	{
+
+	}
+
+	@Override
+	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
+	{
+		Tessellator var5 = Tessellator.instance;
+		if (var5.isDrawing)
+		{
+			// Call draw() to clear out any existing block drawing.
+			var5.draw();
+		}
+
+		int mode = var5.drawMode;
+
+		var5.startDrawing(GL11.GL_TRIANGLES);
+
+		GL11.glDisable(GL11.GL_CULL_FACE);
+
+		if (block.blockID == marchingcubes.instance.fillerCubeBlock.blockID)
+		{
+			// YZ Plane
+			fillerBlockRenderer(var5, 0, world, x, y, z, block, modelId, renderer);
+
+			// XY Plane
+			fillerBlockRenderer(var5, 1, world, x, y, z, block, modelId, renderer);
+
+			// XZ Plane
+			fillerBlockRenderer(var5, 2, world, x, y, z, block, modelId, renderer);
+		}
+		else
+			if (block.blockID == marchingcubes.instance.marchingCubeBlock.blockID)
+			{
+				marchingCubeBlockRenderer(var5, 0, world, x, y, z, block, modelId, renderer);
+			}
+
+		var5.draw();
+
+		GL11.glEnable(GL11.GL_CULL_FACE);
+
+		// After changing to GL_TRIANGLES, we need 
+		// to change back to the previous draw mode
+		var5.startDrawing(mode);
+		return true;
+	}
+
+	@Override
+	public boolean shouldRender3DInInventory()
+	{
+		return false;
+	}
+
+	@Override
+	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer)
+	{
+
+	}
+
 }
